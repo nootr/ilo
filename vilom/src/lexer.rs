5d328e41cc1a61d4cbba2b121427e87b6c4c5ce2
Joris Hartog <jorishartog@hotmail.com>
2022-10-19 15:08:00 +0200

    Add global token
    Our first token has been tokenized! 🎉

--- vilom/src/lexer.rs
+++ vilom/src/lexer.rs
@@ -1,6 +1,31 @@
-pub enum Token {}
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum Token {
+    Global(String),
+}
+
+impl FromStr for Token {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut words = s.split(' ');
+        match words.next() {
+            Some("global") => Ok(Token::Global(words.next().unwrap().to_string())),
+            _ => Err(()),
+        }
+    }
+}
 
 pub fn tokenize(program: &str) -> Result<Vec<Token>, &str> {
-    println!("program: {}", program);
-    Ok(Vec::new())
+    let mut tokens = Vec::new();
+
+    for line in program.lines() {
+        if let Ok(t) = Token::from_str(line.trim()) {
+            tokens.push(t);
+        }
+    }
+
+    println!("tokens: {:?}", tokens);
+    Ok(tokens)
 }
