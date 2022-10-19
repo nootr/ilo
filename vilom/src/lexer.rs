e3dbd8ffa3fad2d52f3a0d1f0e28b5fc0558190c
Joris Hartog <jorishartog@hotmail.com>
2022-10-19 20:33:08 +0200

    Implement Registers and Section/Push/Pop tokens

--- vilom/src/lexer.rs
+++ vilom/src/lexer.rs
@@ -1,8 +1,56 @@
 use std::str::FromStr;
 
 #[derive(Debug)]
+pub enum Register {
+    Rax,
+    Rbx,
+    Rcx,
+    Rdx,
+    R8,
+    R9,
+    R10,
+    Rsi,
+    Rdi,
+}
+
+impl FromStr for Register {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 2 {
+            return Err(());
+        }
+
+        match &s[..2] {
+            "r8" => return Ok(Register::R8),
+            "r9" => return Ok(Register::R9),
+            _ => {}
+        }
+
+        if s.len() < 3 {
+            return Err(());
+        }
+
+        match &s[..3] {
+            "rax" => Ok(Register::Rax),
+            "rbx" => Ok(Register::Rbx),
+            "rcx" => Ok(Register::Rcx),
+            "rdx" => Ok(Register::Rdx),
+            "rsi" => Ok(Register::Rsi),
+            "rdi" => Ok(Register::Rdi),
+            "r10" => Ok(Register::R10),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum Token {
+    Comment,
     Global(String),
+    Section(String),
+    Push(Register),
+    Pop(Register),
 }
 
 impl FromStr for Token {
@@ -11,7 +59,23 @@ impl FromStr for Token {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut words = s.split(' ');
         match words.next() {
+            Some(";") => Ok(Token::Comment),
             Some("global") => Ok(Token::Global(words.next().unwrap().to_string())),
+            Some("section") => Ok(Token::Section(words.next().unwrap().to_string())),
+            Some("push") => match words.next() {
+                Some(register_name) => match Register::from_str(register_name) {
+                    Ok(register) => Ok(Token::Push(register)),
+                    _ => Err(()),
+                },
+                _ => Err(()),
+            },
+            Some("pop") => match words.next() {
+                Some(register_name) => match Register::from_str(register_name) {
+                    Ok(register) => Ok(Token::Pop(register)),
+                    _ => Err(()),
+                },
+                _ => Err(()),
+            },
             _ => Err(()),
         }
     }
